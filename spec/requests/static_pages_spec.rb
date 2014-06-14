b6cf0050e5cc5006d5477b6abe60830a4e4e0cd2
shoji-k <koba.s0125@gmail.com>
2014-06-14 17:05:03 +0900

    use shared_example in rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,38 +4,36 @@ describe "StaticPages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
-    it { should have_content('SecondApp') }
-    it { should have_title(full_title('')) }
+
+    let(:heading)    { 'SecondApp' }
+    let(:page_title) { '' }
+    it_should_behave_like "all static pages"
+
     it { should_not have_title('| Home') }
   end
   describe "Help page" do
     before { visit help_path }
-    it "should have the content Help" do
-      expect(page).to have_content('Help')
-    end
-    it "should have the title 'Help'" do
-      expect(page).to have_title('Ruby on Rails Tutorial SecondApp | Help')
-    end
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
+    it_should_behave_like "all static pages"
   end
   describe "About page" do
     before { visit about_path }
-    it "should have the content About Us" do
-      expect(page).to have_content('About us')
-    end
-    it "should have the title 'About'" do
-      expect(page).to have_title('Ruby on Rails Tutorial SecondApp | About us')
-    end
+    let(:heading)    { 'About us' }
+    let(:page_title) { 'About us' }
+    it_should_behave_like "all static pages"
   end
   describe "contact page" do
     before { visit contact_path }
-    it "should have the content contact" do
-      expect(page).to have_content('contact')
-    end
-    it "should have the title 'contact'" do
-      expect(page).to have_title('Ruby on Rails Tutorial SecondApp | contact')
-    end
+    let(:heading)    { 'contact' }
+    let(:page_title) { 'contact' }
+    it_should_behave_like "all static pages"
   end
-
 end
