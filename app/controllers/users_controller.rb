3408af65b1270e376dfd06d2f53df49cbbe839ac
shoji-k <koba.s0125@gmail.com>
2014-06-21 17:03:03 +0900

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,24 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    if @user.save
+      flash[:success] = "Welcome to the Second App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+
 end
